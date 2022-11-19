585cd07c1397d77ed2400de6b4c839b39cdfde6c
alymurtazamemon <alimurtaza-assistant@outlook.com>
2022-11-19 23:56:53 +0500

    add title and subtitle

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
 import "./App.css";
 
 function App() {
-    return <h1>Hello world!</h1>;
+    return (
+        <div className="pt-4">
+            <div className="">
+                <h1 className="text-sky-400 text-center font-black text-8xl">
+                    Ethers Wallet
+                </h1>
+                <p className="text-center text-sky-300 font-thin text-xl pt-2">
+                    A decentralized way to deposit, withdraw and transfer funds.
+                </p>
+            </div>
+        </div>
+    );
 }
 
 export default App;
