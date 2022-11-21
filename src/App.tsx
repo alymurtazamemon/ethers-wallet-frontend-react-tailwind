11a1b9d0d15381ada9a939e870a477fcc5cb3180
alymurtazamemon <alimurtaza-assistant@outlook.com>
2022-11-21 23:13:05 +0500

    design input field

--- src/App.tsx
+++ src/App.tsx
@@ -13,6 +13,13 @@ function App() {
                 </p>
             </div>
             <ConnectButton />
+            <div className="flex justify-center mt-24">
+                <input
+                    className="w-1/3 px-4 py-3 text-sky-300 placeholder:text-sky-300 placeholder:italic bg-transparent border-2 border-sky-400 rounded-md text-sm shadow-sm placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-2 focus:ring-sky-500"
+                    type="text"
+                    placeholder="0.1 ether"
+                />
+            </div>
         </div>
     );
 }
